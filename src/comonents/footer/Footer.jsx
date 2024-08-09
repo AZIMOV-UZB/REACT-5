7d96c0b54908638aa7d16b1a5fcdadf67a605a94
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-08-08 22:05:54 -0700

    react

--- src/comonents/footer/Footer.jsx
+++ src/comonents/footer/Footer.jsx
@@ -3,21 +3,42 @@ import React from 'react'
 const Footer = () => {
   return (
     <div >
-            <div className='w-32 container mx-auto justify-start'>
-        <p className="text-3xl font-bold text-left "> clickhose<hr className="hrr" /></p>
+            <div className='w-32 container mx-auto '>
+                <div className='text-left w-full  justify-start'>
+        <p className="text-3xl font-bold  "> clickhose<hr className="hrr" /></p>
         </div>
-        <div className='footer__wrapper container mx-auto py-14'>
+        </div>
+        <div className='footer__wrapper container mx-auto py-14 flex flex-wrap	 justify-between'>
 
-<div className='footer__card mx-4'>
-    <p className='footer__title text-2xl font-bold'></p>
-    <p className='text-xl font-normal' ></p>
-    <p className='text-xl font-normal' ></p>
-    <p className='text-xl font-normal' ></p>
-    <p className='text-xl font-normal' ></p>
-    <p className='text-xl font-normal' ></p>
+<div className='footer__card mx-4  w-80'>
+    <p className='footer__title text-2xl font-bold'>Информация</p>
+    <p className='text-xl font-normal' >Все права защищены © 2015 интернет-магазин AIKEA.BY Беларусь.
+    </p>
+    <p className='text-xl font-normal' >УНП 191828159 ИП Парейко В.С., регистрационный номер в торговом реестре 158299
+    </p>
+    <p className='text-xl font-normal' >Политика конфиденциальности</p>
 
 </div>
 
+<div className='footer__card mx-4  w-80'>
+    <p className='footer__title text-2xl font-bold'>Меню</p>  
+    <p className='text-xl font-normal' >Главная | Каталог | Товары в наличии</p>
+    <p className='text-xl font-normal' >Скидки | Популярное | Вдохновение</p>
+    <p className='text-xl font-normal' >Доставка | Услуги | Условия</p>
+    <p className='text-xl font-normal' >Контакты | Вставить | Cписок покупок</p>
+    <p className='text-xl font-normal' >Корзина | Личный кабинет | Избранное</p>
+
+</div>
+
+<div className='footer__card mx-4  w-80'>
+    <p className='footer__title text-2xl font-bold'>Контакты</p>
+    <p className='text-xl font-normal' >г.Минск</p>
+    <p className='text-xl font-normal' >Ул. Первомайская, Д. 1, Кв. 43</p>
+    <p className='text-xl font-normal' >+ 375 434 847 28 84</p>
+    <p className='text-xl font-normal' >+ 375 448 473 09 48</p>
+    <p className='text-xl font-normal' >ikeaekspress@gmail.com</p>
+
+</div>
         </div>
 
     </div>
